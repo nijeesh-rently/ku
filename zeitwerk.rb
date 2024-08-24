56ee4cd212bb4ccc91e167b9577c7c4340d75420
Nijeesh Joshy <nijeesh.k@rently.com>
2024-08-24 17:28:06 +0530

    fixed issues with path names

--- zeitwerk.rb
+++ zeitwerk.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
 require 'zeitwerk'
+require 'dry/cli'
+
+CURRENT_DIR = File.dirname(__FILE__)
 
 loader = Zeitwerk::Loader.new
 
 loader.inflector.inflect('cli' => 'CLI')
 
-loader.push_dir('./src/')
+loader.push_dir File.join(CURRENT_DIR, 'src')
 
 loader.setup
